debdfbe08bb238b0db2636f9dee227d3937d38a0
Razvp <razvanpetru@users.noreply.github.com>
2020-03-14 14:32:29 +0200

    first commit

--- app.js
+++ app.js
@@ -20,7 +20,7 @@ app.use(shopRoutes);
 app.use('/public', express.static(path.join(__dirname, 'public')));
 
 app.get('/', (req, res, next) => {
-    res.sendFile(__dirname + '/views/home.html');
+    res.sendFile(__dirname + '/index.html');
 });
 
 app.use((req, res, next) => {
