e810fccf0e827851a181e35522422facb67f64cc
Razvp <razvanpetru@users.noreply.github.com>
2020-03-14 14:22:49 +0200

    Changed Port

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const path = require('path');
 
+const PORT = process.env.PORT || 3000;
 const app = express();
 
 const adminRoutes = require('./routes/admin');
@@ -26,4 +27,4 @@ app.use((req, res, next) => {
     res.status(404).sendFile(__dirname + '/views/404.html');
 });
 
-app.listen(3000);
+app.listen(PORT);
